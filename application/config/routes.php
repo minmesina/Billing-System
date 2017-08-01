49043c5cbb63a3d0374284d5644e3d19494103e9
Jasmin Mesina <jasmin.mesina@gmail.com>
2017-08-01 11:47:36 +0800

    Added home page

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,11 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$route['posts/index'] = 'posts/index';
-$route['posts/create'] = 'posts/create';
-$route['posts/update'] = 'posts/update';
-$route['posts/(:any)'] = 'posts/view/$1';
-$route['posts'] = 'posts/index';
 
 $route['default_controller'] = 'pages/view';
 
@@ -13,6 +8,5 @@ $route['projects'] = 'projects/index';
 
 $route['clients'] = 'clients/index';
 
-$route['(:any)'] = 'pages/view/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
---
+++ application/views/pages/home.php
@@ -0,0 +1,2 @@
+<h2><?= $title ?></h2>
+<p>Welcome to the Billing System</p>
